75f0de7bc4e4f05cd4b777836150bea2d5a0b7ab
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-08-13 14:58:34 -0700

    completed

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-// TODO
+var groceryList = ['chicken skewers', 'macadamian nuts', 'jerky'];
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList todos={groceryList}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <GroceryListItem  todo={todo} />
+    )}
+  </ul>
+)
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      done: false
+    };
+  }
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.done ? '700' : '400'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)} onMouseOut={this.onListItemHover.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+
+
+
+// var GroceryList = () => (
+//   <ul> 
+//     <ChickenSkewers />
+//     <MacadamianNuts />
+//   </ul>
+// );
+
+// var ChickenSkewers = () => (
+//   <li> Chicken Skewers  </li>
+// );
+// var MacadamianNuts = () => (
+//   <li>Macadamian Nuts </li>
+// );
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
